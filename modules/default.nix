753586eff44afe99cba38188fb8b0ec54d34fbc7
RobertD95 <robertparson95@protonmail.com>
2026-02-18 22:00:01 -0800

    Dark Theme 1st Commit

--- modules/default.nix
+++ modules/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./hyprland.nix
+    ./theme.nix
     ./packages
     ./launchers
   ];
---
+++ modules/theme.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+{
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.kdePackages.breeze-icons;
+    name = "Breeze-Light";
+    size = 72;
+  };
+
+  gtk = {
+    enable = true;
+
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Dark";
+    };
+
+    iconTheme = {
+      package = pkgs.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+
+    font = {
+      name = "Sans";
+      size = 11;
+    };
+  };
+}
