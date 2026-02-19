e1d819b40066394d738e601e1bf1156d923a5c1a
RobertD95 <robertparson95@protonmail.com>
2026-02-18 22:09:32 -0800

    Commit 3

--- modules/theme.nix
+++ modules/theme.nix
@@ -1,29 +1,26 @@
 { pkgs, ... }:
 {
-  home.pointerCursor = {
-    gtk.enable = true;
-    # x11.enable = true;
-    package = pkgs.kdePackages.breeze-icons;
-    name = "Breeze-Light";
-    size = 72;
+	home.pointerCursor = {
+		gtk.enable = true;
+		# x11.enable = true;
+		package = pkgs.kdePackages.breeze-icons;
+		name = "Breeze-Light";
+		size = 72;
   };
+	gtk = {
+		enable = true;
+		theme = {
+			package = pkgs.breeze-gtk; # Breeze GTK theme package
+			name = "Breeze";           # Use Breeze theme
+		};
+		iconTheme = {
+			package = pkgs.breeze-icons; # Breeze icons package
+			name = "Breeze";             # Breeze icon theme
+		};
+  		font = {
+		name = "Sans";
+		size = 11;
+  		};
+	};
 
-  gtk = {
-    enable = true;
-
-    theme = {
-      package = pkgs.kdePackages.breeze;
-      name = "Breeze Dark";
-    };
-
-    iconTheme = {
-      package = pkgs.adwaita-icon-theme;
-      name = "Adwaita";
-    };
-
-    font = {
-      name = "Sans";
-      size = 11;
-    };
-  };
 }
