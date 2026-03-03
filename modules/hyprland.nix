2973c75f976adfd29c2e794406ebeccd443b252e
RobertD95 <robertparson95@protonmail.com>
2026-03-03 12:08:03 -0800

    Update hyprland.nix

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,17 +1,5 @@
 { config, inputs, ... }:
-let
-  hypr = inputs.hyprland.packages.x86_64-linux;
-in
 {
-  xdg.configFile."uwsm/env".source =
-    "${config.home.sessionVariablesPackage}/etc/profile.d/hm-session-vars.sh";
-  wayland.windowManager.hyprland = {
-    enable = true;
-    package = null;
-    portalPackage = null;
-    systemd.variables = [ "-all" ];
-  };
-
   services = {
     hyprpaper.enable = true;
     hyprsunset.enable = true;
