70c2d742728a0681ce99271fcb3fa7204c0bd910
RobertD95 <robertparson95@protonmail.com>
2026-03-07 00:39:46 -0800

    Update hyprland.nix

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -4,14 +4,16 @@
                 hyprpaper = { 
                         enable = true;
                         package = pkgs.hyprpaper;
-                        settings = [
+                        settings = {
+                                preload = [
                                         "/home/robby/Pictures/Games/TLoZ/OOT.jpg"
                                         "/home/robby/Pictures/Games/FFXIV/FireWorks.png"
-                        ];
-                        wallpapaer = [
-                                "DP-1, /home/robby/Pictures/Games/TLOZ/OOT.jpg"
-                                "HDMI-A-1, /home/robby/Pictures/Games/FFXIV/FireWorks.png"
-                        ];
+                                ];
+                                wallpapaer = [
+                                        "DP-1, /home/robby/Pictures/Games/TLOZ/OOT.jpg"
+                                        "HDMI-A-1, /home/robby/Pictures/Games/FFXIV/FireWorks.png"
+                                ];
+                        };
                 };
                 hyprsunset = { enable = true; package = pkgs.hyprsunset; };
                 hyprpolkitagent = { enable = true; package = pkgs.hyprpolkitagent; };
