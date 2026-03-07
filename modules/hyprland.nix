8e6b2ac2426d9f11cd04718166ea6740342bbf1a
RobertD95 <robertparson95@protonmail.com>
2026-03-06 20:34:50 -0800

    Update hyprland.nix

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,7 +1,5 @@
 { pkgs, ... }:
 {
-        wayland.windowManager.hyprland = { enable = true; package = null; systemd.enable = false; xwayland.enable = true; };
-        
         services = {
                 hyprpaper = { enable = true; package = pkgs.hyprpaper; };
                 hyprsunset = { enable = true; package = pkgs.hyprsunset; };
