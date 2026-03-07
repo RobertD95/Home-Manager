86f8d4d95978fb3b4d762ef1f97ee7dc70002577
RobertD95 <robertparson95@protonmail.com>
2026-03-07 01:33:45 -0800

    Update hyprland.nix

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,27 +1,7 @@
 { pkgs, lib, ... }:
 {
-        wayland.windowManager.hyprland  = {
-                enable = true;
-                systemd = {
-                        enable = true;
-                        variables = ["--all"];
-                };
-                xwayland.enable = true;
-        };
         services = {
-                hyprpaper = { 
-                        enable = true;
-#                       settings = {
-#preload = [
-#                                       "$HOME/Pictures/Games/TLoZ/OOT.jpg"
-#                                       "$HOME/Pictures/Games/FFXIV/FireWorks.png"
-#                               ];
-#                               wallpapaer = [
-#                                       "DP-1, $HOME/Pictures/Games/TLOZ/OOT.jpg"
-#                                       "HDMI-A-1,$HOME/Pictures/Games/FFXIV/FireWorks.png"
-#                               ];
-#                       };
-               };
+                hyprpaper = { enable = true; package = pkgs.hyprpaper; };
                 hyprsunset = { enable = true; package = pkgs.hyprsunset; };
                 hyprpolkitagent = { enable = true; package = pkgs.hyprpolkitagent; };
         };
