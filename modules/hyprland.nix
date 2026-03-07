cf8f8fa720a64b838aa76b9bcf712118b15e2aad
RobertD95 <robertparson95@protonmail.com>
2026-03-07 00:36:37 -0800

    Wallpaper Fix

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,7 +1,18 @@
 { pkgs, ... }:
 {
         services = {
-                hyprpaper = { enable = true; package = pkgs.hyprpaper; };
+                hyprpaper = { 
+                        enable = true;
+                        package = pkgs.hyprpaper;
+                        settings = [
+                                        "/home/robby/Pictures/Games/TLoZ/OOT.jpg"
+                                        "/home/robby/Pictures/Games/FFXIV/FireWorks.png"
+                        ];
+                        wallpapaer = [
+                                "DP-1, /home/robby/Pictures/Games/TLOZ/OOT.jpg"
+                                "HDMI-A-1, /home/robby/Pictures/Games/FFXIV/FireWorks.png"
+                        ];
+                };
                 hyprsunset = { enable = true; package = pkgs.hyprsunset; };
                 hyprpolkitagent = { enable = true; package = pkgs.hyprpolkitagent; };
         };
