0524e90e77dd7a0051413c7f9b49b3ef518a62ee
RobertD95 <robertparson95@protonmail.com>
2026-03-10 10:26:22 -0700

    Discord Removed
    Discord is now used as a flatpak.

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -5,7 +5,6 @@
     ./appimage
     ./blockbench
     ./clonehero
-    ./discord
     ./gnome
     ./kde
     ./librewolf
--- modules/packages/discord/default.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, inputs, ... }:
-{
-  home.packages = [
-    pkgs.discord
-  ];
-}
