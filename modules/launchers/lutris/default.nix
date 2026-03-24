7dfada746ed3544981cc91c6cffa6c08b052f993
RobertD95 <robertparson95@protonmail.com>
2026-03-24 00:37:53 -0700

    Latest version of Lutris
    If it decides to work

--- modules/launchers/lutris/default.nix
+++ modules/launchers/lutris/default.nix
@@ -1,9 +1,16 @@
 { pkgs, inputs, ... }:
+let
+        ver = "0.5.22";
+        lutris = builtins.fetchTarball {
+                url = "https://github.com/lutris/lutris/archive/refs/tags/v${ver}.tar.gz";
+                sha256 ="0h73zp82xz7q1iaa03vpiw4biia1ig93aa1lcq7r2h69yyg68qz2";
+        };
+in
 {
   programs = {
     lutris = {
       enable = true;
-      package = pkgs.lutris;
+      package = lutris;
     };
   };
 }
