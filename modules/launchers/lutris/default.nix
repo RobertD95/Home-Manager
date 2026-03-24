b5cad275079616c7cb135f837e3973280a71dbb3
RobertD95 <robertparson95@protonmail.com>
2026-03-24 00:42:23 -0700

    Update default.nix

--- modules/launchers/lutris/default.nix
+++ modules/launchers/lutris/default.nix
@@ -1,16 +1,17 @@
-{ pkgs, inputs, ... }:
+{ pkgs, inputs, config, ... }:
 let
         ver = "0.5.22";
-        lutris = builtins.fetchTarball {
+        nixpkgs = builtins.fetchTarball {
                 url = "https://github.com/lutris/lutris/archive/refs/tags/v${ver}.tar.gz";
                 sha256 ="0h73zp82xz7q1iaa03vpiw4biia1ig93aa1lcq7r2h69yyg68qz2";
         };
+        lutris = import nixpkgs { config = config.nixpkgs.config; };
 in
 {
   programs = {
     lutris = {
       enable = true;
-      package = lutris;
+      package = lutris.lutris;
     };
   };
 }
