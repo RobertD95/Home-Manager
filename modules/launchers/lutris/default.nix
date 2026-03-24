8a684b069fb455166f142a4c40c77d6fbd5c4ebf
RobertD95 <robertparson95@protonmail.com>
2026-03-24 00:55:56 -0700

    Update default.nix

--- modules/launchers/lutris/default.nix
+++ modules/launchers/lutris/default.nix
@@ -7,8 +7,8 @@
                                 src = pkgs.fetchFromGitHub {
                                         owner = "lutris";
                                         repo = "lutris";
-                                        rev = "master";
-                                        sha256 = "";
+                                        rev = "v0.5.22";
+                                        sha256 = "sha256-4mNknvfJQJEPZjQoNdKLQcW4CI93D6BUDPj8LtD940A=";
                                 };
                         });
                 };
