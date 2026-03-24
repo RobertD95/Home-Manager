e018a6a62439240a565b72f506217af715220e07
RobertD95 <robertparson95@protonmail.com>
2026-03-24 00:49:14 -0700

    Update default.nix

--- modules/launchers/lutris/default.nix
+++ modules/launchers/lutris/default.nix
@@ -1,16 +1,16 @@
 { pkgs, inputs, config, ... }:
-let
-        ver = "0.5.22";
-        lutris = builtins.fetchTarball {
-                url = "https://github.com/lutris/lutris/archive/refs/tags/v${ver}.tar.gz";
-                sha256 ="0h73zp82xz7q1iaa03vpiw4biia1ig93aa1lcq7r2h69yyg68qz2";
-        };
-in
 {
-  programs = {
-    lutris = {
-      enable = true;
-      package = lutris;
-    };
-  };
+        programs = {
+                lutris = {
+                        enable = true;
+                        package = pkgs.lutris.overrideAttrs (old: {
+                                src = pkgs.fetchFromGitHub {
+                                        owner = "lutris";
+                                        repo = "lutris";
+                                        rev = "master";
+                                        sha256 = "";
+                                };
+                        });
+                };
+        };
 }
